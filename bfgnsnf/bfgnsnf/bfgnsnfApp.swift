e850b69e264cd53af45c0cac4dde635637471d8b
ShikamotoRG <shikamoto.rg@gmail.com>
2023-06-23 00:33:20 +0300

    hi

---
+++ bfgnsnf/bfgnsnf/bfgnsnfApp.swift
@@ -0,0 +1,17 @@
+//
+//  bfgnsnfApp.swift
+//  bfgnsnf
+//
+//  Created by Rafael on 23/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct bfgnsnfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
